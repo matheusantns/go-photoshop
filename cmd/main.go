847a8cd508b42924963aa3158a140ccb942cf507
Matheus Antunes <matheus.antunes@tarkuss.com.br>
2024-04-01 21:09:47 -0400

    initialize htmx project

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"io"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	psd "github.com/matheusantns/go-photoshop/internal"
 )
 
 type Templates struct {
@@ -14,6 +16,10 @@ type Templates struct {
 
 type Count struct {
 	Count int
+}
+
+type Layers struct {
+	Layers []psd.Layer
 }
 
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
@@ -31,15 +37,34 @@ func main() {
 	e.Use(middleware.Logger())
 
 	count := Count{Count: 0}
+	layers := Layers{Layers: make([]psd.Layer, 0)}
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		if len(layers.Layers) == 0 {
+			fmt.Println("Layers vazia")
+		}
+
+		return c.Render(200, "index", struct {
+			Count  Count
+			Layers Layers
+		}{count, layers})
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "count", count)
+	e.POST("/psd/get-fields", func(c echo.Context) error {
+		psdPath := c.FormValue("psd")
+		layersResult, err := psd.HandlePSD(psdPath)
+		if err != nil {
+			return fmt.Errorf("deu erro - %w", err)
+		}
+
+		layers.Layers = layersResult
+
+		if len(layers.Layers) == 0 {
+			fmt.Println("Layers vazia")
+		}
+
+		return c.Render(200, "index", layers)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
